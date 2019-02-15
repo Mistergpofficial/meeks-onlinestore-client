b2e5afd58386b58727f1fd3ff60ff1391fdb660d
chuky4real <ehuriahgodspower@gmail.com>
2019-02-15 13:01:57 +0100

    new commit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 var path = require('path');
+const history = require('connect-history-api-fallback');
 
 
 const app = express();
-app.use('/', express.static(__dirname + '/dist'));
+//app.use('/', express.static(__dirname + '/dist'));
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 app.use(express.static('static'));
 //Serves all the request which includes /images in the url from Images folder
 app.use('/images', express.static(__dirname + '/images'));
